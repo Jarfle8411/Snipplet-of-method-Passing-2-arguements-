22ff35d4b5b3e7b9eef26e28ff4a4177648e9a0e
Jarvis <39226019+Jarfle8411@users.noreply.github.com>
2019-01-26 17:31:46 -0500

    Add files via upload
    Here's a program where I create a method passing 2 arguments.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Passing_2_Arguements
+{
+    class Simple_method_passing_two_arguements
+    {
+        static void Main(string[] args)
+        {
+            //initialize two varibles
+            string name = "John Doe";
+            int age = 32;
+
+            HereCome2Arguements(name, age);
+        }
+
+        public static void HereCome2Arguements(string whoIsThis, int yearsLiving)
+        {
+            Console.WriteLine("Hello my name is {0} and my age is {1}", whoIsThis, yearsLiving);
+
+        }
+
+    }
+}
